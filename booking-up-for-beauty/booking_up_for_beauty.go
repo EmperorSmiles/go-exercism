0f683161a7fc32d8fee403bf249de608be561d7b
EmperorSmiles <iamfavoursmiles@gmail.com>
2025-01-28 23:26:53 +0100

    Updated

--- booking-up-for-beauty/booking_up_for_beauty.go
+++ booking-up-for-beauty/booking_up_for_beauty.go
@@ -1,15 +1,40 @@
 package booking
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
-	panic("Please implement the Schedule function")
+	
+	layout := "1/2/2006 15:04:05"
+	
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		fmt.Println("Error parsing date:", err)
+		return time.Time{}
+	}
+
+	return t
+}
+
+func hasPassedTime(date string) time.Time {
+	layout := "January 2, 2006 15:04:05"
+	t, err := time.Parse(layout, date)
+
+	if err != nil {
+		fmt.Println("Error parsing date:", err)
+		return time.Time{}
+	}
+	return t
+
 }
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
-	panic("Please implement the HasPassed function")
+	t := hasPassedTime(date).Before(time.Now())
+	return t
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
