1c70e5f23a6b1f44b8ac180e7a2aff42dace4e59
EmperorSmiles <iamfavoursmiles@gmail.com>
2025-01-29 13:50:17 +0100

    DRYing up mu code

--- booking-up-for-beauty/booking_up_for_beauty.go
+++ booking-up-for-beauty/booking_up_for_beauty.go
@@ -7,47 +7,40 @@ import (
 
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
-	
-	layout := "1/2/2006 15:04:05"
-	
-	t, err := time.Parse(layout, date)
-	if err != nil {
-		fmt.Println("Error parsing date:", err)
-		return time.Time{}
+	layouts := []string{
+		"January 2, 2006 15:04:05", 
+		"1/2/2006 15:04:05",
+		"Monday, January 2, 2006 15:04:05",
 	}
 
-	return t
-}
-
-func hasPassedTime(date string) time.Time {
-	layout := "January 2, 2006 15:04:05"
-	t, err := time.Parse(layout, date)
-
-	if err != nil {
-		fmt.Println("Error parsing date:", err)
-		return time.Time{}
+	for _, layout := range layouts {
+		t, err := time.Parse(layout, date)
+		if err == nil {
+			return t
+		}
 	}
-	return t
-
+	fmt.Println("Error: Unable to parse date with any known format")
+	return time.Time{}
 }
+
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
-	t := hasPassedTime(date).Before(time.Now())
-	return t
+	return Schedule(date).Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
 func IsAfternoonAppointment(date string) bool {
-	panic("Please implement the IsAfternoonAppointment function")
+	return Schedule(date).Hour() >= 12 && Schedule(date).Hour() < 18 
 }
 
 // Description returns a formatted string of the appointment time.
 func Description(date string) string {
-	panic("Please implement the Description function")
+	return fmt.Sprintf("You have an appointment on %v", Schedule(date).Format("Monday, January 2, 2006, at 15:04."))
 }
+
 
 // AnniversaryDate returns a Time with this year's anniversary.
 func AnniversaryDate() time.Time {
-	panic("Please implement the AnniversaryDate function")
+	return time.Date(time.Now().Year(), time.September, 15, 0, 0, 0, 0, time.UTC )
 }
