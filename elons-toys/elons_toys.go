1d473fac86298ca177f203dc27a62adc881b925e
EmperorSmiles <iamfavoursmiles@gmail.com>
2025-02-04 02:47:03 +0100

    All test passed

--- elons-toys/elons_toys.go
+++ elons-toys/elons_toys.go
@@ -1,12 +1,36 @@
 package elon
 
+import "fmt"
+
 // TODO: define the 'Drive()' method
+func (c *Car) Drive() {
+	if c.battery >= c.batteryDrain {
+		c.distance += c.speed
+		c.battery -= c.batteryDrain
+	}
+}
 
 // TODO: define the 'DisplayDistance() string' method
+func (c *Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
 
 // TODO: define the 'DisplayBattery() string' method
+func (c *Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
+}
 
 // TODO: define the 'CanFinish(trackDistance int) bool' method
+func (c *Car) CanFinish(trackDistance int) bool {
+	maxDrive := c.battery / c.batteryDrain
+	maxDistance := maxDrive * c.speed
+
+	if maxDistance >= trackDistance {
+		return true
+	}
+	return false
+}
+
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
