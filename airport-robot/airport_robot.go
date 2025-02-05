07f727f38279b27ee2a9d4132b00bbaa78fb7912
EmperorSmiles <iamfavoursmiles@gmail.com>
2025-02-05 02:13:16 +0100

    test passed

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,40 @@
 package airportrobot
 
+import (
+	"fmt"
+)
+
 // Write your code here.
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+
+func SayHello(name string, g Greeter) string {
+	language := g.LanguageName()
+	greeting := g.Greet(name)
+	return fmt.Sprintf("I can speak %s: %s!", language, greeting)
+}
+
+type Italian struct {}
+type Portuguese struct {}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s", name)
+}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s", name)
+}
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
+
